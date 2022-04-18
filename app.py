934d4bdf81b8b0fcce0c60ca5fa2c2096c74e083
r3stl3ss <rod.pires@yandex.ru>
2022-04-18 14:50:45 +0300

    created first templates, added model and db and figured out how to create templates

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///links.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+class Link(db.Model):
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    original_link = db.Column(db.String(), nullable=False)
+    hashed_link = db.Column(db.String(), nullable=False)
+    is_deleted = db.Column(db.Boolean)
+    date_of_creation = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Link %r>" % self.id
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
