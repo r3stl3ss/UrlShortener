a26751f776b2f8a2c6583e063bad143e73d6dc46
r3stl3ss <rod.pires@yandex.ru>
2022-05-29 14:57:36 +0300

    minor fixes, deleted all debug things and added docstrings

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from hashids import Hashids
@@ -30,39 +28,35 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-
 @app.route("/link_generator", methods=['POST', 'GET'])
 def generate():
-    all_except_deleted = Link.query.filter(Link.is_deleted == False)
+    '''Метод, сокращающий ссылку в зависимости от id этой ссылки в базе данных'''
     if request.method == 'POST':
         original_link = request.form['original_link']
-        results = [id[0] for id in Link.query.with_entities(Link.id).all()]
-        hashed_link = hashids.encode(max(results) + 1)
+        results = [id[0] for id in Link.query.with_entities(Link.id).all()] # Получаем массив всех id в базе данных в данный момент...
+        hashed_link = hashids.encode(max(results) + 1) # ... и для новой ссылки берём максимальный и увеличиваем его на один
         link = Link(original_link=original_link, hashed_link=hashed_link, is_deleted=False)
         try:
             db.session.add(link)
             db.session.commit()
-            shown_url = "127.0.0.1:5000/" + hashed_link
+            shown_url = "127.0.0.1:5000/" + hashed_link # Тут создаём ссылку, которую потом будем отображать, и если бы приложение было не на локалхосте, следовало бы поменять эту строку
             return render_template('shortened_link_page.html', short_url=shown_url, link="redir/" + hashed_link)
         except Exception as e:
-            print(e.with_traceback())
             return "Error while shortening link"
     else:
-        return render_template('generator.html')
+        return render_template('generator.html') # Это на случай, когда мы просто переходим на страницу, а не вкидываем вдобавок свою ссылку
 
 
 @app.route("/get_all_links")
 def get_hash():
-    all_except_deleted = Link.query.filter(Link.is_deleted == False)
+    """Метод, достающий из базы данных все ссылки, с сортировкой по количеству нажатых раз по убыванию"""
+    all_except_deleted = Link.query.filter(Link.is_deleted == False).order_by(Link.times_clicked.desc())
     return render_template("allLinks.html", links=all_except_deleted)
 
 
 @app.route("/redir/<hashed_link>")
 def redirect_to_short(hashed_link):
+    """Метод=перенаправитель, достающий из БД полученный хэш, сверяющий его с ссылкой и перенаправляющий на оригинал ссылки"""
     link_id = hashids.decode(hashed_link)[0]
     if link_id:
         orig = Link.query.filter(Link.id == link_id).filter(Link.is_deleted == False).first()
@@ -77,6 +71,7 @@ def redirect_to_short(hashed_link):
 
 @app.route("/del/<hashed_link>")
 def delete_link(hashed_link):
+    """Метод, осуществляющий soft delete ссылки, после чего она перестаёт отображаться на главной странице, и без доступа к БД к ней обратиться нельзя"""
     link_id = hashids.decode(hashed_link)[0]
     link = Link.query.filter(Link.id == link_id).filter(Link.is_deleted == False).first()
     link.is_deleted = True
