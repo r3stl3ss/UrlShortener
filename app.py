9c02bc1c5b95696e5bf6804bc38a4584f7049ab0
r3stl3ss <rod.pires@yandex.ru>
2022-06-01 16:00:41 +0300

    validator for links fixed

--- app.py
+++ app.py
@@ -34,24 +34,40 @@ def generate():
     """Метод, сокращающий ссылку в зависимости от id этой ссылки в базе данных"""
     if request.method == 'POST':
         original_link = request.form['original_link']
-        full_link_http = 'http://' + original_link # это на случай, если на вход пришла сокращённая ссылка типа habr.com
-        full_link_https = 'https://' + original_link # для валидации используется библиотека requests, отправляющая GET запрос на адрес
-        if requests.get(original_link).status_code == 200 \
-        or requests.get(full_link_http).status_code == 200 \
-        or requests.get(full_link_https).status_code == 200: # однако запрос не доходит до сокращённых ссылок, поэтому я их искусственно удлиняю
-            results = [id[0] for id in Link.query.with_entities(Link.id).all()]  # Получаем массив всех id в базе данных в данный момент...
-            hashed_link = hashids.encode(max(results) + 1)  # ... и для новой ссылки берём максимальный и увеличиваем его на один
-            link = Link(original_link=original_link, hashed_link=hashed_link, is_deleted=False)
-            try:
-                db.session.add(link)
-                db.session.commit()
-                shown_url = "127.0.0.1:5000/" + hashed_link  # Тут создаём ссылку, которую потом будем отображать, и если бы приложение было не на локалхосте, следовало бы поменять эту строку
-                return render_template('shortened_link_page.html', short_url=shown_url, link="redir/" + hashed_link)
-            except:
-                return "Error while shortening link"
+        full_link_http = 'http://' + original_link  # это на случай, если на вход пришла сокращённая ссылка типа habr.com
+        full_link_https = 'https://' + original_link  # для валидации используется библиотека requests, отправляющая GET запрос на адрес
+        results = [id[0] for id in Link.query.with_entities(
+            Link.id).all()]  # Получаем массив всех id в базе данных в данный момент...
+        hashed_link = hashids.encode(
+            max(results) + 1)  # ... и для новой ссылки берём максимальный и увеличиваем его на один
+        if original_link.startswith('http://') or original_link.startswith('https://'): # это на случай, если мы получаем полноценную ссылку, с http в начале
+            if requests.get(original_link).status_code == 200:  # однако запрос не доходит до сокращённых ссылок, поэтому я их искусственно удлиняю
+                link = Link(original_link=original_link, hashed_link=hashed_link, is_deleted=False)
+            else:
+                flash("Invalid URL", category='invalid_link')  # и если ссылка нерабочая, выводится сообщение и происходит редирект обратно на генератор
+                return render_template('generator.html')
         else:
-            flash("Invalid URL", category='invalid_link') # и если ссылка нерабочая, выводится сообщение и происходит редирект обратно на генератор
-            return render_template('generator.html')
+            try:
+                check_request_http = requests.get(full_link_http) # это на случай, когда сайт доступен по протоколу http
+                if check_request_http.status_code == 200:
+                    link = Link(original_link=full_link_http, hashed_link=hashed_link, is_deleted=False)
+                else:
+                    check_request_https = requests.get(full_link_https) # а это в случае, если вдруг сайт доступен ТОЛЬКО по https
+                    if check_request_https.status_code == 200:
+                        link = Link(original_link=full_link_https, hashed_link=hashed_link, is_deleted=False)
+                    else:
+                        flash("Invalid URL", category='invalid_link')
+                        return render_template('generator.html')
+            except:
+                flash("Invalid URL", category='invalid_link')
+                return render_template('generator.html')
+        try:
+            db.session.add(link)
+            db.session.commit()
+            shown_url = "127.0.0.1:5000/" + hashed_link  # Тут создаём ссылку, которую потом будем отображать, и если бы приложение было не на локалхосте, следовало бы поменять эту строку
+            return render_template('shortened_link_page.html', short_url=shown_url, link="redir/" + hashed_link)
+        except:
+            return "Error while shortening link"
     else:
         return render_template('generator.html')  # Это на случай, когда мы просто переходим на страницу, а не вкидываем вдобавок свою ссылку
 
